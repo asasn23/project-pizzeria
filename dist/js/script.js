baf082a562eeaa68937775783f2d449d2b8c3014
Kamil Trzcinski <asasn233@gmail.com>
2023-12-15 18:51:16 +0100

    add initOrderForm

--- dist/js/script.js
+++ dist/js/script.js
@@ -60,6 +60,8 @@
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
       console.log('new Product:', thisProduct);
     }
     renderInMenu(){
@@ -92,9 +94,33 @@
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
- }
 
+    initOrderForm() {
+      const thisProduct = this;
+      console.log(thisProduct.initOrderForm);
 
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder(){
+      const thisProduct = this;;
+      console.log(thisProduct.processOrder);
+
+    }
+  }
 
   const app = {
     initMenu: function(){
