ec1bbcbf31ce6d80810f9eda402382f397c35b2a
Kamil Trzcinski <asasn233@gmail.com>
2024-01-04 23:21:04 +0100

    add booking and switching pages

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,43 @@
 import {settings, select, classNames, templates} from './settings.js';
 import Product from './components/product.js';
 import Cart from './components/cart.js';
-
+import Booking from './components/booking.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[0].id;
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+    for (let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    for (let link of thisApp.navLinks){
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+    }
+  },
+
   initMenu: function(){
     const thisApp = this;
     console.log('thisApp.data:', thisApp.data);
@@ -40,6 +74,12 @@ const app = {
     });
   },
 
+  initBooking: function() {
+    const thisApp = this;
+    thisApp.bookingWidgetElement = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(thisApp.bookingWidgetElement);
+  },
+
   init: function(){
     const thisApp = this;
     console.log('*** App starting ***');
@@ -47,9 +87,10 @@ const app = {
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);
-
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
